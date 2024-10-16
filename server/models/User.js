5a02b0d690c09fec09582204180957a798992603
Christophe73-netizen <128191211+Christophe73-netizen@users.noreply.github.com>
2024-10-16 12:13:18 +0000

    push and commit

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true }
+  //phone: { type: String, required: true }
 }, { timestamps: true });
 
 // Méthode pour hacher le mot de passe avant de sauvegarder
